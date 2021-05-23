7794f114cc9a45975c42fb6c32e4a8835892d9da
neiljaywarner <njwandroid@gmail.com>
2021-05-23 17:39:32 -0500

    very slight code cleanup and upgrade dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Firebase Analytics Demo',
+      title: 'Hope Signin Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
