cd7ec18e6d9f84c7a7f769e4625a6ff8ee3841ee
neiljaywarner <njwandroid@gmail.com>
2021-06-07 20:34:48 -0500

    added flutterw instead of path bc pinning, added integration test hello world from doc as failing test

---
+++ test_driver/integration_test.dart
@@ -0,0 +1,10 @@
+import 'package:integration_test/integration_test_driver.dart';
+
+Future<void> main() => integrationDriver();
+
+/*
+flutter drive \
+  --driver=test_driver/integration_test.dart \
+  --target=integration_test/app_test.dart \
+  -d "0A081JECB07111"
+ */
